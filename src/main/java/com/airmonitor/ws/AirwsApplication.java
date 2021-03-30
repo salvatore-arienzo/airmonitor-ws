cc1ece03c35a5306250d60e8e432ac1d698cbefe
salvatore-arienzo <s.arienzo@studenti.unisa.it>
2021-03-30 17:23:33 +0200

    setup changed

--- src/main/java/com/airmonitor/ws/AirwsApplication.java
+++ src/main/java/com/airmonitor/ws/AirwsApplication.java
@@ -1,13 +1,22 @@
 package com.airmonitor.ws;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AirwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirwsApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
