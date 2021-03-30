427ce14a3c56c443df777f8e97be21a168817e3b
Salvatore Arienzo <32364699+salvatore-arienzo@users.noreply.github.com>
2021-03-30 16:32:48 +0200

    first commit

---
+++ src/main/java/com/airmonitor/ws/AirwsApplication.java
@@ -0,0 +1,13 @@
+package com.airmonitor.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airmonitor/ws/AirwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airmonitor.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
